ce19d07434dad5112640386c933ef5558219e2d1
17727921123 <caiang.666>
2020-08-06 23:44:40 +0800

    SpringCloudConfigServer

---
+++ src/main/java/com/lsm/scc/server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.lsm.scc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+//启用SpringCloudConfig服务
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsm/scc/server/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lsm.scc.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
