00a197fe76b00072547776d2faaf24b1e30a1e21
17727921123 <caiang.666>
2020-08-07 20:18:39 +0800

    SpringCloudConfig使用Eureka

--- src/main/java/com/lsm/scc/server/SpringCloudConfigServerApplication.java
+++ src/main/java/com/lsm/scc/server/SpringCloudConfigServerApplication.java
@@ -3,10 +3,13 @@ package com.lsm.scc.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 //启用SpringCloudConfig服务
 @EnableConfigServer
+
 public class SpringCloudConfigServerApplication {
 
     public static void main(String[] args) {
